b4381aadcabf9584888ac6a170490906aeef0240
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-04-09 14:56:27 +0300

    Added day as an instance variable

--- src/fi/helsinki/cs/scheduler3000/model/Event.java
+++ src/fi/helsinki/cs/scheduler3000/model/Event.java
@@ -6,6 +6,8 @@ package fi.helsinki.cs.scheduler3000.model;
 
 
 public class Event {
+	
+	private Weekday.Day day;
 
 	private String title;
 	private String location;
@@ -13,18 +15,19 @@ public class Event {
 	private int startTime;
 	private int endTime;
 	
-	public Event(int startTime, int endTime){
+	public Event(Weekday.Day day, int startTime, int endTime){
+		this.day = day;
 		this.startTime = startTime;
 		this.endTime = endTime;
 	}
 	
-	public Event(String title, int startTime, int endTime){
-		this(startTime, endTime);
+	public Event(Weekday.Day day, String title, int startTime, int endTime){
+		this(day, startTime, endTime);
 		this.setTitle(title);
 	}
 	
-	public Event(String title, String location, int startTime, int endTime){
-		this(title, startTime, endTime);
+	public Event(Weekday.Day day, String title, String location, int startTime, int endTime){
+		this(day, title, startTime, endTime);
 		this.setLocation(location);
 	}
 	
@@ -34,6 +37,14 @@ public class Event {
 
 	public void setLocation(String location) {
 		this.location = location;
+	}
+	
+	public Weekday.Day getDay() {
+		return day;
+	}
+	
+	public void setDay(Weekday.Day day) {
+		this.day = day;
 	}
 
 	public int getStartTime() {
