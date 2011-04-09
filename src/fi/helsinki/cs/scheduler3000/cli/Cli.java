616fdd396db1640b1ab83a0f80ff81cbaf45bf93
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-04-09 13:23:24 +0300

    Moved creation of Schedule to NewSchedule

--- src/fi/helsinki/cs/scheduler3000/cli/Cli.java
+++ src/fi/helsinki/cs/scheduler3000/cli/Cli.java
@@ -30,8 +30,6 @@ public class Cli extends CliCommand {
 	private static ObjectOutputStream objectOutput;
 	private static ObjectInputStream objectInput;
 	
-	
-	
 	public void run(){
 
 		Character foo;
@@ -74,7 +72,8 @@ public class Cli extends CliCommand {
 				break;
 				
 			case 'n':
-				newScheduleDialog();
+				CliCommand newSchedule = new NewSchedule(schedule);
+				newSchedule.run();
 				break;
 				
 			case 'o':
@@ -106,42 +105,6 @@ public class Cli extends CliCommand {
 		return ret;
 	}
 
-	private static void newScheduleDialog() {
-		String in = null;
-		ArrayList<Day> dates = new ArrayList<Day>();
-
-		System.out.println("Enter the period this schedule is for:");
-		printPrompt();
-		String period = input.nextLine();
-
-		System.out.println("Give dates you want to include in the scedule");
-		System.out.println("Stop giving the dates by entering \""+endCommand+"\"");
-		System.out.println("One at a time, please");
-
-		do {
-			Helpers.printDates();
-			Helpers.printSelection(dates);
-			printPrompt();
-			in = input.nextLine().trim();
-
-			if (in.toLowerCase().equals(endCommand)){
-				break;
-			}
-			else {
-				Day day = Helpers.getDay(in.trim());
-				if( day != null ) {
-					dates.add(day);
-				}
-			}
-
-		} while (true);
-
-		System.out.print("Creating schedule...");
-		schedule = new Schedule(dates, period);
-
-		System.out.println("ok!");
-
-	}
 
 	private static boolean open(String filename) {
 		
