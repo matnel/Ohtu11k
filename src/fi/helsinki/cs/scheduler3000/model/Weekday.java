691f32cedb77571c4019336260a01244a11dba5a
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-04-09 12:25:56 +0300

    Made smarter use of enums

--- src/fi/helsinki/cs/scheduler3000/model/Weekday.java
+++ src/fi/helsinki/cs/scheduler3000/model/Weekday.java
@@ -6,41 +6,41 @@ package fi.helsinki.cs.scheduler3000.model;
 
 
 import java.util.HashMap;
+import java.util.GregorianCalendar;
 
 
 public class Weekday {
 
-	public static enum Day { MON, TUE, WED, THU, FRI, SAT, SUN };	
-	public static HashMap<Integer, Day> intToEnumMap = new HashMap<Integer, Day>();
-	public static HashMap<Day, Integer> enumToIntMap = new HashMap<Day, Integer>();
-	public static HashMap<Day, String> longNameMap = new HashMap<Day, String>();
-	
-	// make handy hashmaps  
-	static {
-		intToEnumMap.put(1, Day.MON);
-		intToEnumMap.put(2, Day.TUE);
-		intToEnumMap.put(3, Day.WED);
-		intToEnumMap.put(4, Day.THU);
-		intToEnumMap.put(5, Day.FRI);
-		intToEnumMap.put(6, Day.SAT);
-		intToEnumMap.put(7, Day.SUN);
+	public static enum Day {
+		MON ("Monday", GregorianCalendar.MONDAY),
+		TUE ("Tuesday", GregorianCalendar.TUESDAY),
+		WED ("Wednesday", GregorianCalendar.WEDNESDAY),
+		THU ("Thursday", GregorianCalendar.THURSDAY),
+		FRI ("Friday", GregorianCalendar.FRIDAY),
+		SAT ("Saturday", GregorianCalendar.SATURDAY),
+		SUN ("Sunday", GregorianCalendar.SUNDAY);
 		
-		enumToIntMap.put(Day.MON, 1);
-		enumToIntMap.put(Day.TUE, 2);
-		enumToIntMap.put(Day.WED, 3);
-		enumToIntMap.put(Day.THU, 4);
-		enumToIntMap.put(Day.FRI, 5);
-		enumToIntMap.put(Day.SAT, 6);
-		enumToIntMap.put(Day.SUN, 7);
-		
-		longNameMap.put(Day.MON, "Monday");
-		longNameMap.put(Day.TUE, "Tuesday");
-		longNameMap.put(Day.WED, "Wednesday");
-		longNameMap.put(Day.THU, "Thursday");
-		longNameMap.put(Day.FRI, "Friday");
-		longNameMap.put(Day.SAT, "Saturday");
-		longNameMap.put(Day.SUN, "Sunday");
-	}
+		private String name;
+		private int calendarValue;
 	
+		Day(String name, int calendarValue) {
+			this.name = name;
+			this.calendarValue = calendarValue;
+		}
+		
+		public String getName() {
+			return this.name;
+		}
+		
+		public int getCalendarDay() {
+			return this.calendarValue;
+		}
+		
+		public String toString() {
+			return this.getName();
+		}
+	
+	};	
+
 
 }
