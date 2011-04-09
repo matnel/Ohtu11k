110dd275fd10d329a04f344e146d13f64bda8505
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-04-09 16:22:23 +0300

    Reflecting new implementation of Event

--- tests/tests/EventTest.java
+++ tests/tests/EventTest.java
@@ -10,6 +10,7 @@ import org.junit.* ;
 
 import fi.helsinki.cs.scheduler3000.*;
 import fi.helsinki.cs.scheduler3000.model.Event;
+import fi.helsinki.cs.scheduler3000.model.Weekday.Day;
 
 public class EventTest extends TestCase{
 	
@@ -17,7 +18,7 @@ public class EventTest extends TestCase{
 	
 	@Before
 	public void setUp() {
-		event = new Event("08","10","title", "testlaboratory");
+		event = new Event( Day.MON, "title", "testlaboratory", 8, 12);
 	}
 	
 	@After
@@ -26,54 +27,26 @@ public class EventTest extends TestCase{
 	}
 
 	@Test
-	public void test_constructor() {
-		
-	    Event E = new Event("10","12") ;
-	    assertTrue(E.getStartTime() != null);
-	    assertTrue(E.getEndTime() != null ) ;
-	}
-
-	@Test
-	public void test_constructor2() {
-	    Event E2 = new Event("10","12", "title") ;
-	    assertTrue(E2.getStartTime() != null);
-	    assertTrue(E2.getEndTime() != null ) ;
-	    assertTrue(E2.getTitle() != null);
+	public void testGetters() {
+		assertEquals(Day.MON, event.getDay() );
+		assertEquals( "title", event.getTitle() );
+		assertEquals( "testlaboratory", event.getLocation() );
+		assertEquals( 8, event.getStartTime() );
+		assertEquals( 12, event.getEndTime() );
 	}
 	
 	@Test
-	public void test_constructor3() {
-	    Event E3 = new Event("12","14", "Ohtu", "Auditory A111") ;
-	    assertTrue(E3.getStartTime() != null);
-	    assertTrue(E3.getEndTime() != null ) ;
-	    assertTrue(E3.getTitle() != null);
-	    assertTrue(E3.getLocation() != null);
-	}
-	
-	@Test
-	public void test_setStartTime(){
-		event.setStartTime("10");
-		assertEquals("10", event.getStartTime());
-		try{
-			event.setStartTime("09");	
-			fail("Expecting IllegalArgumentException");
-		}catch(IllegalArgumentException r){
-			// Everything went better than expected
-		}
-	}
-	
-	@Test
-	public void test_setEndTime(){
-		event.setEndTime("12");
-		assertEquals("12", event.getEndTime());
-		try{
-			event.setEndTime("25");
-			fail("Expecting IllegalArgumentException");
-		}catch(IllegalArgumentException r){
-			// Everything went better than expected
-		}
-		
-		
+	public void testSetters() {
+		event.setDay( Day.TUE );
+		assertEquals( Day.TUE, event.getDay() );
+		event.setTitle("title1");
+		assertEquals( "title1", event.getTitle() );
+		event.setLocation("test");
+		assertEquals( "test", event.getLocation() );
+		event.setStartTime(10);
+		assertEquals( 10, event.getStartTime() );
+		event.setEndTime(14);
+		assertEquals( 14, event.getEndTime() );
 	}
 	 
 }
