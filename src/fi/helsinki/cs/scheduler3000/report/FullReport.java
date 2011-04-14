105924f224eb1e362354ff5ffe40bb9b9c0c70e4
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-04-14 16:39:37 +0300

    Removed toString() from Report and moved it to FullReport

--- src/fi/helsinki/cs/scheduler3000/report/FullReport.java
+++ src/fi/helsinki/cs/scheduler3000/report/FullReport.java
@@ -6,13 +6,33 @@ package fi.helsinki.cs.scheduler3000.report;
 
 import java.util.HashMap;
 
+import fi.helsinki.cs.scheduler3000.model.Event;
 import fi.helsinki.cs.scheduler3000.model.Schedule;
+import fi.helsinki.cs.scheduler3000.model.Weekday;
 
 
 public class FullReport extends Report {
 
 	public FullReport(Schedule schedule, HashMap<String, Object> options) {
 		super(schedule, options);
+	}
+	
+	// FIXME: should this be elsewhere?
+	public String toString(){
+		String res = "";
+		
+		for (Weekday.Day day : schedule.getDays() ){
+			res += day + ":\n";
+			res += "----\n";
+			for (Event event : schedule.getEventsOn(day) ){
+				res += event.getTitle();
+				res += "\nat " + event.getStartTime() + "-" + event.getEndTime();
+				res += "\nin " + event.getLocation();
+			}
+			res += "\n\n";
+		}
+		
+		return res;
 	}
 
 }
--- src/fi/helsinki/cs/scheduler3000/report/Report.java
+++ src/fi/helsinki/cs/scheduler3000/report/Report.java
@@ -11,7 +11,7 @@ import fi.helsinki.cs.scheduler3000.model.Event;
 import fi.helsinki.cs.scheduler3000.model.Schedule;
 import fi.helsinki.cs.scheduler3000.model.Weekday;
 
-public class Report {
+public abstract class Report {
 
 	// visible to subclasses
 	protected Schedule schedule;
@@ -20,24 +20,6 @@ public class Report {
 	public Report(Schedule schedule, HashMap<String, Object> options) {
 		this.schedule = schedule;
 		this.options = options;
-	}
-	
-	// FIXME: should this be elsewhere?
-	public String toString(){
-		String res = "";
-		
-		for (Weekday.Day day : schedule.getDays() ){
-			res += day + ":\n";
-			res += "----\n";
-			for (Event event : schedule.getEventsOn(day) ){
-				res += event.getTitle();
-				res += "\nat " + event.getStartTime() + "-" + event.getEndTime();
-				res += "\nin " + event.getLocation();
-			}
-			res += "\n\n";
-		}
-		
-		return res;
 	}
 	
 }
